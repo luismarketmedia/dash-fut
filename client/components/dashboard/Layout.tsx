72907b59762f9aa75375f2d5f5fe007e27b40cc5
Luis Aldrighi <jose.luis@marketmedia.com.br>
2025-09-11 14:57:49 -0300

    Update Layout.tsx

--- client/components/dashboard/Layout.tsx
+++ client/components/dashboard/Layout.tsx
@@ -1,5 +1,6 @@
-import { ReactNode } from "react";
 ("use client");
+
+import { ReactNode } from "react";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
